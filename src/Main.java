fca916d17afbe212b86a60a9635580afeb397660
ibmsc <Ibm.sc1409@gmail.com>
2022-06-10 19:53:54 +0300

    2 коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Home Work 3");
+
+
+        // Задач №1.
+
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
