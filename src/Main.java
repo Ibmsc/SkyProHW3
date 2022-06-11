e379022b36812dd07ce8e1b52b56eb5e79ad827e
ibmsc <Ibm.sc1409@gmail.com>
2022-06-11 18:55:55 +0300

    7 коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Home Work 3");
+        System.out.println("Домашняя работа № 3");
 
 
         System.out.println("Задача №1");
